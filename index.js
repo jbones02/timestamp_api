46fb87ba57e1e001bb9b72be0e5975fe68cb4cd9
Jaryd Bones <77517310+jbones02@users.noreply.github.com>
2025-01-13 21:12:00 -0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
-// index.js
-// where your node app starts
-
 // init project
 var express = require('express');
 var app = express();
 
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
 var cors = require('cors');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
@@ -55,7 +50,6 @@ app.get("/api/:date", (req, res) => {
   res.status(200).send(dateReturnObj);
 })
 
-// Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
